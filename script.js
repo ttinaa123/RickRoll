0dfb1ef04312c4a5e3abf4999226a266e7556c67
ttinaa123 <119396435+ttinaa123@users.noreply.github.com>
2023-03-28 09:35:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const coolButton = document.querySelector
+('.js-cool')
+const dontButton = document.querySelector
+('.js-dont')
+
+coolButton.addEventListener('mouseover'() => {
+    window.open('https://www.youtube.com/watch?v=2GLe6hEC0zg&ab_channel=ScorpioMusic')
+})
+
+dontButton.addEventListener('mouseover'() => {
+   window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ ')
+})
